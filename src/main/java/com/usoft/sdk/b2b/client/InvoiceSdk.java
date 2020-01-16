f4c64bca739931cec0153fd74503158f0c7ccbe9
wangcanyi <wangcanyi@usoftchina.com>
2020-01-16 15:05:02 +0800

    应付发票上传状态 与 明细字段

--- src/main/java/com/usoft/sdk/b2b/client/InvoiceSdk.java
+++ src/main/java/com/usoft/sdk/b2b/client/InvoiceSdk.java
@@ -1,14 +1,9 @@
 package com.usoft.sdk.b2b.client;
 
 import com.usoft.b2b.external.erp.invoice.api.entity.APBill;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.GetSaleAPBillsReq;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.GetSaleAPBillsResp;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.SaveApBillsReq;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.SaveApBillsResp;
-import com.usoft.b2b.external.erp.order.api.protobuf.UpdateSaleDownStatusResp;
+import com.usoft.b2b.external.erp.invoice.api.protobuf.*;
 import com.usoft.sdk.b2b.utils.HttpUtil;
 import com.usoft.sdk.b2b.utils.ProtoBufUtil;
-import io.grpc.protobuf.ProtoUtils;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -22,43 +17,61 @@ import java.util.Map;
 public class InvoiceSdk extends BaseSdk {
 
 
-    public InvoiceSdk(String baseUrl, String secretId, String secretKey) {
-        super(baseUrl, secretId, secretKey);
-    }
+	public InvoiceSdk(String baseUrl, String secretId, String secretKey) {
+		super(baseUrl, secretId, secretKey);
+	}
 
-    public InvoiceSdk(String baseUrl, String secretId, String secretKey, int timeout) {
-        super(baseUrl, secretId, secretKey, timeout);
-    }
+	public InvoiceSdk(String baseUrl, String secretId, String secretKey, int timeout) {
+		super(baseUrl, secretId, secretKey, timeout);
+	}
 
-    /**
-     * 买家ERP-将ERP的应付发票写到平台
-     *
-     * @param req
-     * @return
-     */
-    public SaveApBillsResp saveApBills(SaveApBillsReq req) throws IOException {
-        String url = baseUrl + "/erp/purchase/APBill";
-        Map<String, String> params = generateSignature(url, null);
-        url = HttpUtil.getPath(url, params);
-        Map<String, String> fromData = new HashMap<>();
-        fromData.put("data", ProtoBufUtil.toJSON(req.getDataList()));
-        HttpUtil.doPost(url, fromData, timeout);
-        return SaveApBillsResp.newBuilder().build();
-    }
+	/**
+	 * 买家ERP-将ERP的应付发票写到平台
+	 *
+	 * @param req
+	 * @return
+	 */
+	public SaveApBillsResp saveApBills(SaveApBillsReq req) throws IOException {
+		String url = baseUrl + "/erp/purchase/APBill";
+		Map<String, String> params = generateSignature(url, null);
+		url = HttpUtil.getPath(url, params);
+		Map<String, String> fromData = new HashMap<>();
+		fromData.put("data", ProtoBufUtil.toJSON(req.getDataList()));
+		HttpUtil.doPost(url, fromData, timeout);
+		return SaveApBillsResp.newBuilder().build();
+	}
 
-    /**
-     * 卖家ERP-从平台获取未下载的客户应付票据
-     * @param req
-     * @return
-     * @throws IOException
-     */
-    public GetSaleAPBillsResp getSaleAPBills(GetSaleAPBillsReq req) throws IOException {
-        String url = baseUrl + "/erp/sale/APBill";
-        Map<String, String> params = generateSignature(url, null);
-        String respJson = HttpUtil.doGet(url, params, timeout);
-        List<APBill> list = ProtoBufUtil.toProtoBufList(APBill.newBuilder().build(), respJson);
-        GetSaleAPBillsResp.Builder resp = GetSaleAPBillsResp.newBuilder();
-        resp.addAllData(list);
-        return resp.build();
-    }
+	/**
+	 * 卖家ERP-从平台获取未下载的客户应付票据
+	 *
+	 * @param req
+	 * @return
+	 * @throws IOException
+	 */
+	public GetSaleAPBillsResp getSaleAPBills(GetSaleAPBillsReq req) throws IOException {
+		String url = baseUrl + "/erp/sale/APBill";
+		Map<String, String> params = generateSignature(url, null);
+		String respJson = HttpUtil.doGet(url, params, timeout);
+		List<APBill> list = ProtoBufUtil.toProtoBufList(APBill.newBuilder().build(), respJson);
+		GetSaleAPBillsResp.Builder resp = GetSaleAPBillsResp.newBuilder();
+		resp.addAllData(list);
+		return resp.build();
+	}
+
+	/**
+	 * 平台的客户应付票据传到供应商ERP之后，修改平台里面的客户应付票据的上传状态
+	 *
+	 * @param req
+	 * @return
+	 * @throws IOException
+	 */
+	public OnSaleAPBillDownSuccessResp onSaleAPBillDownSuccess(OnSaleAPBillDownSuccessReq req) throws IOException {
+		String url = baseUrl + "/erp/sale/APBill";
+		Map<String, String> params = generateSignature(url, null);
+		url = HttpUtil.getPath(url, params);
+		Map<String, String> fromData = new HashMap<>(1);
+		fromData.put("data", req.getIdStr());
+		HttpUtil.doPost(url, fromData, timeout);
+		return OnSaleAPBillDownSuccessResp.newBuilder().build();
+	}
 }
--- src/test/java/com/usoft/sdk/b2b/InvoiceSdkTest.java
+++ src/test/java/com/usoft/sdk/b2b/InvoiceSdkTest.java
@@ -1,10 +1,7 @@
 package com.usoft.sdk.b2b;
 
 import com.usoft.b2b.external.erp.invoice.api.entity.APBill;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.GetSaleAPBillsReq;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.GetSaleAPBillsResp;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.SaveApBillsReq;
-import com.usoft.b2b.external.erp.invoice.api.protobuf.SaveApBillsResp;
+import com.usoft.b2b.external.erp.invoice.api.protobuf.*;
 import com.usoft.sdk.b2b.client.InvoiceSdk;
 import com.usoft.sdk.b2b.utils.ProtoBufUtil;
 import org.junit.jupiter.api.Test;
@@ -17,30 +14,37 @@ import java.io.IOException;
  */
 public class InvoiceSdkTest {
 
-    /**
-     * 测试地址
-     */
-    private InvoiceSdk invoiceSdk = new InvoiceSdk("http://test-b2b.uuzcc.cn", "10041559", "2c1ea089876b796fe050007f01002ea6");
+	/**
+	 * 测试地址
+	 */
+	private InvoiceSdk invoiceSdk = new InvoiceSdk("http://test-b2b.uuzcc.cn", "10041559", "2c1ea089876b796fe050007f01002ea6");
 
-    /**
-     * 正式地址
-     */
+	/**
+	 * 正式地址
+	 */
 //	private ProductSdk productSdk = new ProductSdk("http://b2b-api.usoftchina.com", "10041559", "2c1ea089876b796fe050007f01002ea6");
+	@Test
+	public void updateSaleDownStatus() throws IOException {
+		SaveApBillsReq.Builder req = SaveApBillsReq.newBuilder();
+		APBill.Builder builder = APBill.newBuilder();
+		builder.setAbId(123);
+		req.addData(builder);
+		SaveApBillsResp resp = invoiceSdk.saveApBills(req.build());
+		System.out.println(ProtoBufUtil.toJSON(resp));
+	}
 
-    @Test
-    public void updateSaleDownStatus() throws IOException {
-        SaveApBillsReq.Builder req = SaveApBillsReq.newBuilder();
-        APBill.Builder builder = APBill.newBuilder();
-        builder.setAbId(123);
-        req.addData(builder);
-        SaveApBillsResp resp = invoiceSdk.saveApBills(req.build());
-        System.out.println(ProtoBufUtil.toJSON(resp));
-    }
+	@Test
+	public void updateProducts() throws IOException {
+		GetSaleAPBillsReq.Builder req = GetSaleAPBillsReq.newBuilder();
+		GetSaleAPBillsResp resp = invoiceSdk.getSaleAPBills(req.build());
+		System.out.println(ProtoBufUtil.toJSON(resp));
+	}
 
-    @Test
-    public void updateProducts() throws IOException {
-        GetSaleAPBillsReq.Builder req = GetSaleAPBillsReq.newBuilder();
-        GetSaleAPBillsResp resp = invoiceSdk.getSaleAPBills(req.build());
-        System.out.println(ProtoBufUtil.toJSON(resp));
-    }
+	@Test
+	public void onSaleAPBillDownSuccess() throws IOException {
+		OnSaleAPBillDownSuccessReq.Builder req = OnSaleAPBillDownSuccessReq.newBuilder();
+		req.setIdStr("123");
+		OnSaleAPBillDownSuccessResp resp = invoiceSdk.onSaleAPBillDownSuccess(req.build());
+		System.out.println(ProtoBufUtil.toJSON(resp));
+	}
 }
